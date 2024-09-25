8b251c62f82a899d03c64476f6cc9a5a1dcd0f91
ryanmendi <ryanmendes112233@gmail.com>
2024-09-24 22:14:09 -0300

    read by, update e delete

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use (
 app.use(express.json())
 
 //rota 1 
-app.get('/', (reg, res)=>{
+app.get('/', (req, res)=>{
     res.json({mensage: "funciona"})
 })
 
@@ -22,7 +22,9 @@ app.post('/person', async(req, res)=> {
     const {name, salary, approved} = req.body;
 
     const person = {
-        approved,
+        name, 
+        salary, 
+        approved
     }
 
     try{
@@ -38,6 +40,69 @@ app.get("/person", async (req, res) =>{
     try{
         const peaple = await Person.find()
         res.status(200).json(peaple)
+    }catch(error){
+        res.status(500).json({erro: error})
+    }
+})
+
+//read by id
+app.get("/person/:id", async (req, res)=>{
+    const id = req.params.id
+    try{
+        const peaple = await Person.findOne({_id: id})
+
+        if(!peaple){
+            res.status(422).json({message: "Usuário não encontrado!"})
+            return
+        }
+
+    res.status(200).json(peaple)
+    
+    } catch(error){
+        res.status(500).json({erro: error})
+    }
+})
+
+//update 
+app.patch("/person/:id", async (req, res)=>{
+    const id = req.params.id
+
+    const {name, salary, approved} = req.body
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+
+    try{
+        const updatePerson = await Person.updateOne({_id: id}, person)
+
+        if(updatePerson.matchedCount === 0){
+            res.status(422).json({message: "O usuário não foi encontrado!"})
+            return
+        }
+        res.status(200).json(Person)
+    } catch(error){
+        res.status(500).json({erro: eroor})
+    }
+})
+
+//delete 
+app.delete("/person/:id", async (req, res)=>{
+
+    const id = req.params.id
+
+    const person = await Person.findOne({_id: id})
+
+    if(!person){
+        res.status(422).json({menssage: "Usuário não encontrado!"})
+        return
+    }
+
+    try{
+        await Person.deleteOne({_id: id})
+        res.status(200).json({menssage: "Usuário removido com sucesso!"})
     }catch(error){
         res.status(500).json({erro: error})
     }
